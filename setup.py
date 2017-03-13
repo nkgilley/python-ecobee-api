21976c7ac5310c8eb0081729b2822bcd893816ca
Dale Higgs <dale3h@gmail.com>
2017-03-13 14:06:51 -0500

    Update version to 0.0.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='python-ecobee',
-      version='0.0.6',
+      version='0.0.7',
       description='Python API for talking to Ecobee thermostats',
       url='https://github.com/nkgilley/python-ecobee-api',
       author='Nolan Gilley',

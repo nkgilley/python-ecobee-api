5ef0fac31205d35bfff64045f4897e3f9b0014c9
Nolan Gilley <nkgilley@gmail.com>
2017-08-28 09:11:24 -0400

    bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ SOFTWARE.
 """
 
 setup(name='python-ecobee-api',
-      version='0.0.8',
+      version='0.0.9',
       description='Python API for talking to Ecobee thermostats',
       url='https://github.com/nkgilley/python-ecobee-api',
       download_url='https://github.com/nkgilley/python-ecobee-api/archive/0.0.8.tar.gz',

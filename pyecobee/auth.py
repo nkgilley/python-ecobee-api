ac2486e949a05d3801bab7e8be593a6132851578
matt <matthew.deren@gmail.com>
2017-09-18 06:51:39 -0600

    Reducing manual dictionary building...

--- pyecobee/auth.py
+++ pyecobee/auth.py
@@ -1,51 +1,57 @@
-import requests
+import requests, json
 from .rest import Rest
 
 class Auth(object):
     def __init__(self, data):
-        #self.authenticated = False
-        #self.pin = None
-        self.rest = Rest(data)
         self.data = data
+        self.rest = Rest(data)
+        self.request_pin()
+        self.request_tokens()
+
+    def connect(self):
+        return True
+
+    def test_pin(self):
+        ''' Test previously saved PIN '''
 
     def request_pin(self):
         ''' Method to request a PIN from ecobee for authorization '''
 
-        params = {'response_type': 'ecobeePin', 'scope': 'smartWrite', 'client_id': self.data['auth']['api_key']}
-        request = self.rest._get("authorize", params=params)
+        self.data.request_pin()
+        params = self.data.store['rest']['request_pin']['params']
+        url = self.data.store['rest']['request_pin']['url']
+        request = self.rest._get(url, params=json.loads(params))
+        self.data.store['auth']['pin'] = request
 
-        self.data['auth']['code'] = request.json()['authorization_code']
-        self.data['auth']['pin'] = request.json()['ecobeePin']
-        self.data['auth']['pin']['scoope'] = request.json()['scope']
-        self.data['auth']['pin']['expires'] = request.json()['expires_in']
 
     def request_tokens(self):
         ''' Method to request API tokens from ecobee '''
 
-        params = {'grant_type': 'ecobeePin', 'code': self.data['auth']['code'], 'client_id': self.data['auth']['api_key']}
-        request = rest._get("token", params=params)
+        self.data.request_token()
+        params = json.dumps(self.data.store['rest']['request_token']['params'])
+        url = self.data.store['rest']['request_token']['url']
+        request = self.rest._get(url, params=json.loads(params))
 
-        self.data['token']['access'] = request.json()['access_token']
-        self.data['token']['expires'] = request.json()['expires_in']
-        self.data['token']['refresh'] = request.json()['refresh_token']
-        self.data['token']['scope'] = request.json()['scope']
-
+        print request
+        self.data.store['auth']['token'] = request
 
     def refresh_tokens(self):
         ''' Method to refresh API tokens from ecobee '''
 
-        params = {'grant_type': 'refresh_token', 'refresh_token': self.data['token']['refresh'], 'client_id': self.data['auth']['api_key']}
+        params = {'grant_type': 'refresh_token', 'refresh_token': self.data.store['token']['refresh'], 'client_id': self.data.store['auth']['api_key']}
         request = self.rest._post("token", params=params)
 
-        self.data['token']['access'] = request.json()['access_token']
-        self.data['token']['expires'] = request.json()['expires_in']
-        self.data['token']['refresh'] = request.json()['refresh_token']
-        self.data['token']['scope'] = request.json()['scope']
+        print json.dumps(self.data.store['auth'])
+
+        #self.data.store['token']['access'] = request.json()['access_token']
+        #self.data.store['token']['expires'] = request.json()['expires_in']
+        #self.data.store['token']['refresh'] = request.json()['refresh_token']
+        #self.data.store['token']['scope'] = request.json()['scope']
 
     def make_request(self, body, log_msg_action):
         url = 'https://api.ecobee.com/1/thermostat'
         header = {'Content-Type': 'application/json;charset=UTF-8',
-                  'Authorization': 'Bearer ' + self.data['token']['access']}
+                  'Authorization': 'Bearer ' + self.data.store['token']['access']}
         params = {'format': 'json'}
         request = requests.post(url, headers=header, params=params, data=body)
         if request.status_code == requests.codes.ok:

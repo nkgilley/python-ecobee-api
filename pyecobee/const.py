ec7b05b42667315698a9734e2fb37f19e8e8e95d
Mark Coombes <mark@markcoombes.ca>
2019-10-22 22:31:14 -0400

    Refactor python-ecobee-api
    Refactor the library to use a single request interface to the ecobee API, which has the advantage of unifying error handling and debug logging. Added type and return hinting to class methods. Raise InvalidTokenError when ecobee API response indicates that the refresh token has become invalid (signalling to the client that new tokens need to be obtained from the API, i.e. a new auth process needs to be undertaken). This commit also lays the groundwork for converting the library to async, and as a consequence breaks handling of a passed-in config filename (new method will need to be called after the class constructor is called), but this will not affect Home Assistant as auth is now config-flow-based (no config file is used).

--- pyecobee/const.py
+++ pyecobee/const.py
@@ -1,4 +1,7 @@
 """Constants used in this library."""
+import logging
+
+_LOGGER = logging.getLogger("pyecobee")
 
 ECOBEE_ACCESS_TOKEN = "ACCESS_TOKEN"
 ECOBEE_API_KEY = "API_KEY"
@@ -9,3 +12,24 @@ ECOBEE_CONFIG_FILENAME = "ecobee.conf"
 
 ECOBEE_STATE_UNKNOWN = -5002
 ECOBEE_STATE_CALIBRATING = -5003
+ECOBEE_VALUE_UNKNOWN = "unknown"
+
+ECOBEE_BASE_URL = "https://api.ecobee.com"
+ECOBEE_ENDPOINT_AUTH = "authorize"
+ECOBEE_ENDPOINT_TOKEN = "token"
+ECOBEE_ENDPOINT_THERMOSTAT = "thermostat"
+ECOBEE_API_VERSION = "1"
+
+ECOBEE_MODEL_TO_NAME = {
+    "idtSmart": "ecobee Smart",
+    "idtEms": "ecobee Smart EMS",
+    "siSmart": "ecobee Si Smart",
+    "siEms": "ecobee Si EMS",
+    "athenaSmart": "ecobee3 Smart",
+    "athenaEms": "ecobee3 EMS",
+    "corSmart": "Carrier/Bryant Cor",
+    "nikeSmart": "ecobee3 lite Smart",
+    "nikeEms": "ecobee3 lite EMS",
+    "apolloSmart": "ecobee4 Smart",
+    "vulcanSmart": "ecobee4 Smart",
+}
--- pyecobee/errors.py
+++ pyecobee/errors.py
@@ -11,3 +11,7 @@ class ExpiredTokenError(EcobeeError):
     """Raised when ecobee API returns a code indicating expired credentials."""
 
     pass
+
+
+class InvalidTokenError(EcobeeError):
+    """Raised when ecobee API returns a code indicating invalid credentials."""

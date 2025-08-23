ff14a943baf81226677eee5d8c4ede50aa5553b7
Nolan Gilley <nkgilley@gmail.com>
2025-08-23 18:51:48 -0400

    Update setup.py - 0.3.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ SOFTWARE.
 """
 
 setup(name='python-ecobee-api',
-      version='0.3.0',
+      version='0.3.1',
       description='Python API for talking to Ecobee thermostats',
       url='https://github.com/nkgilley/python-ecobee-api',
       author='Nolan Gilley',
